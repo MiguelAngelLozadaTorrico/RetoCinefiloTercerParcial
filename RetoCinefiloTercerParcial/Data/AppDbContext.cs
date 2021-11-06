8757c25b832547145ea5bd08256bf41b21c0373d
MiguelAngelLozadaTorrico <a2020110903@estudiantes.upsa.edu.bo>
2021-11-05 23:03:59 -0400

    Creacion de AppDbContext

---
+++ RetoCinefiloTercerParcial/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetoCinefiloTercerParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetoCinefiloTercerParcial.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Cinefilo> Cinefilo { get; set; }
+        public object Cinefilia { get; internal set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
