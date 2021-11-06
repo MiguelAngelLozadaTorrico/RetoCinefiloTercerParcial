624ad33544cce844c2d1f1357846a2b06295d6fe
MiguelAngelLozadaTorrico <a2020110903@estudiantes.upsa.edu.bo>
2021-11-05 23:04:55 -0400

    Agregacion del configureService en Startup

--- RetoCinefiloTercerParcial/Startup.cs
+++ RetoCinefiloTercerParcial/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RetoCinefiloTercerParcial.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,10 @@ namespace RetoCinefiloTercerParcial
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RetoCinefiloTercerParcial", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+            Configuration.GetConnectionString("MiguelContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
